e8e6135f46eb7864cfe6b20001b6acbfd1ab42ac
Bradley Bruce <Bradleyabruce@outlook.com>
2020-01-08 22:45:52 -0500

    Normalization
    Created new tables:
    - tParkingLotCoordinates
    - tSpotAvailability
    - tSpotImageCoordinate
    Broke existing data out of tables to form these new tables and clean up data, also created primary keys for all tables

--- CreateSpotCheckTables.sql
+++ CreateSpotCheckTables.sql
@@ -1,61 +1,53 @@
 CREATE TABLE tParkingLot(
-	[LotID] [int] IDENTITY(1,1) NOT NULL,
+	[LotID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Address] [varchar](100) NOT NULL,
 	[ZipCode] [int] NOT NULL,
 	[City] [varchar](100) NOT NULL,
 	[State] [varchar](20) NOT NULL,
 	[LotName] [varchar](100) NOT NULL,
 	[ContactID] [int] NULL,
-	[Latitude] [decimal](9, 6) NULL,
-	[Longitude] [decimal](9, 6) NULL,
-	[OpenSpots] [int] NOT NULL,
-	[TotalSpots] [int] NOT NULL 
+	--[OpenSpots] [int] NOT NULL,
+	--[TotalSpots] [int] NOT NULL 
 );
 
 CREATE TABLE tSpot(
-	[SpotID] [int] IDENTITY(1,1) NOT NULL,
+	[SpotID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[FloorNum] [int] NULL,
 	[LotID] [int] NOT NULL,
 	[IsOpen] [bit] NOT NULL,
 	[DeviceID] [int],
-	[TopLeftXCoordinate [int]],
-	[TopLeftYCoordinate [int]],
-	[BottomRightXCoordinate [int]],
-	[BottomRightYCoordinate [int]],
 	[UpdateDate [datetime]]
 );
 	
 CREATE TABLE tLotUsage(
-	[LotUsageID] [int] IDENTITY(1,1) NOT NULL,
+	[LotUsageID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[SpotID] [int] NOT NULL,
 	[Timestamp] [datetime] NOT NULL,
 	[ChangeType] [varchar] NOT NULL
 );
 
 CREATE TABLE tUserInfo(
-	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[UserID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[LastName] [varchar](100) NOT NULL,
 	[FirstName] [varchar](100) NOT NULL,
+	[EmailAddress] [varchar](100) NOT NULL,
 	[UserName] [varchar](100) NOT NULL,
-	[Password] [varchar](100) NOT NULL,
-	[EmailAddress] [varchar](100) NOT NULL
+	[Password] [varchar](100) NOT NULL
 );
 
 CREATE TABLE tDevice(
-	[DeviceID] [int] IDENTITY(1,1) NOT NULL,
+	[DeviceID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[DeviceName] [varchar](100) NOT NULL,
 	[LocalIpAddress] [varchar](100) NOT NULL,
 	[ExternalIpAddress] [varchar](100) NOT NULL,
 	[MacAddress] [varchar](100) NOT NULL,
-	[LotID] [int],
-	[FloorNumber] [int],
 	[LastUpdateDate] [datetime] NOT NULL,
 	[CompanyID] [int] NOT NULL,
 	[TakeNewImage] [bit] DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE tCompany(
-	[CompanyID] [int] IDENTITY(1,1) NOT NULL,
+	[CompanyID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[CompanyName] [varchar](100) NOT NULL,
 	[Address] [varchar](100) NOT NULL,
 	[ZipCode] [int] NOT NULL,
@@ -66,13 +58,36 @@ CREATE TABLE tCompany(
 );
 
 CREATE TABLE tContact(
-	[ContactID] [int] IDENTITY(1,1) NOT NULL,
+	[ContactID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[FirstName] [varchar](100) NOT NULL,
 	[LastName] [varchar](100) NOT NULL,
 	[PhoneNumber] [varchar](15) NOT NULL,
 	[EmailAddress] [varchar](100) NOT NULL,
 	[CompanyID] [int] NOT NULL
 );
+
+Create TABLE tSpotImageCoordinates(
+	[SpotCoordinateID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[SpotID] [int] NOT NULL,
+	[TopLeftXCoordinate] [int] NOT NULL,
+	[TopLeftYCoordinate] [int] NOT NULL,
+	[BottomRightXCoordinate] [int] NOT NULL,
+	[BottomRightYCoordinate] [int] NOT NULL
+);
+
+CREATE TABLE tSpotAvailability(
+	[SpotAvailabilityID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[SpotID] [int] NOT NULL,
+	[IsOpen] [bit] NOT NULL
+);
+
+CREATE TABLE tParkingLotCoordinates(
+	[ParkingLotCoordinateID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[ParkingLotID] [int] NOT NULL,
+	[Latitude] [decimal](9, 6) NULL,
+	[Longitude] [decimal](9, 6) NULL,
+);
+
 
 ALTER TABLE tParkingLot
 ADD Coordinates geography
